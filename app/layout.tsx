e812cc9d7d03b99d8ca51d74836673220c6fc099
rakharrs <rodyox.yoann@gmail.com>
2025-12-04 10:36:27 +0300

    fix: some metadata changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "sonner";
 import { TransitionProvider } from "@/components/TransitionProvider";
 
 export const metadata: Metadata = {
-  title: "portfolio - Rodolphe Yoann RAKOTO-HARISOA",
+  title: "portfolio - Rodolphe Yoann",
   description: "Hi, I am Rodolphe Yoann RAKOTO-HARISOA, a computer science student & full stack developer and here is my portfolio website.",
 };
 
